3f8ea40c7bf6f22f5349d22827a6e0f8896628f2
FiereEinar <nickxylanmelloria@gmail.com>
2024-06-17 07:49:27 +0800

    added landing page

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,9 +1,42 @@
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
+
 export default function Landingpage() {
 	return (
-		<>
-			<section>
-				<h1 className=''>Landing Page</h1>
+		<main className='p-5 flex justify-center items-center'>
+			<section className='flex justify-center items-center max-w-[55rem] p-10 pt-10 flex-wrap md:flex-nowrap shadow-xl border rounded-lg text-center md:text-start'>
+				<div>
+					<h1 className='text-3xl pb-1 font-medium'>
+						Welcome to <span className='text-orange-500'>Blogs</span>.Daily!
+					</h1>
+					<p className=' text-wrap text-center md:text-start'>
+						A place to <span className='text-orange-500'>explore</span> diverse
+						blogs and engage in discussions with fellow readers.{' '}
+						<span className='text-orange-500'>Enjoy</span> your reading
+						experience!
+					</p>
+					<p className='my-1 text-wrap text-center md:text-start'>
+						<span className='text-orange-500'>Sign up now</span> to discover
+						more user features!
+					</p>
+					<div className='flex gap-2 pt-2 justify-center md:justify-start'>
+						<Link to='/signup'>
+							<Button size='sm'>Sign up</Button>
+						</Link>
+						<Link to='/blogs'>
+							<Button size='sm' variant='link'>
+								Start reading
+								<img src='/src/assets/arrow.svg' alt='' />
+							</Button>
+						</Link>
+					</div>
+				</div>
+				<img
+					className='md:w-[28rem] w-[20rem]'
+					src='/src/assets/landing-page-img.png'
+					alt=''
+				/>
 			</section>
-		</>
+		</main>
 	);
 }
