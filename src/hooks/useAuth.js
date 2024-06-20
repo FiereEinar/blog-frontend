f5ef7994e0437bb0a8f1d10657578f4c2c51dc3e
FiereEinar <nickxylanmelloria@gmail.com>
2024-06-20 12:46:14 +0800

    improved useAuth hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,7 @@
 import { getTokenFromLocalStorage } from '@/utils/localstorage';
 import { useState, useEffect } from 'react';
+import { jwtDecode } from "jwt-decode";
+import { getUserById } from '@/api/user';
 
 /**
  * 
@@ -9,7 +11,22 @@ export default function useAuth() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    setIsLoggedIn(getTokenFromLocalStorage() !== null);
+    try {
+      const validate = async () => {
+        const token = getTokenFromLocalStorage().split(' ')[1];
+        const data = jwtDecode(token);
+        const userId = data.user._id;
+
+        // ensure that the user actually exists in the database
+        const userData = await getUserById(userId);
+
+        setIsLoggedIn(userData !== null)
+        console.log(userData);
+      }
+      validate();
+    } catch (err) {
+      console.error('Error authenticating user.', err);
+    }
   }, []);
 
   return isLoggedIn;
