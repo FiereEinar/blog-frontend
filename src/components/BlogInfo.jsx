edc426158416eeb6d03d7cc331fbf9c269683249
FiereEinar <nickxylanmelloria@gmail.com>
2024-06-21 18:59:35 +0800

    updated text rendering

--- src/components/BlogInfo.jsx
+++ src/components/BlogInfo.jsx
@@ -6,10 +6,13 @@ export default function BlogInfo({ blog }) {
 			<p className='text-muted-foreground'>
 				{format(blog.dateAdded, 'MMMM dd, yyyy')}
 			</p>
-			<h1 className='text-2xl text-center font-semibold'>{blog.title}</h1>
+			<h1
+				dangerouslySetInnerHTML={{ __html: blog.title }}
+				className='text-2xl text-center font-semibold'
+			/>
 			<h4 className='text-xl'>{blog.topic.title}</h4>
 			<img className='rounded-sm' src={blog.img.url} alt='blog image' />
-			<p>{blog.text}</p>
+			<p dangerouslySetInnerHTML={{ __html: blog.text }} />
 		</section>
 	);
 }
